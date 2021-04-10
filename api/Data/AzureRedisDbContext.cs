3103359816999828b7bb2d71d55ef97169e43d3d
support <support@dotnetlead.com>
2021-04-09 20:58:47 -0700

    add starting project

---
+++ api/Data/AzureRedisDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Model;
+
+namespace Data
+{
+    public class AzureRedisDbContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+
+        public AzureRedisDbContext(DbContextOptions<AzureRedisDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
